b86da10b7a1f83c8d5018ef7e7d396d729911923
XiongJWei <1481462497@qq.com>
2020-05-20 14:37:31 +0800

    Initial commit

---
+++ le-auth/src/main/java/cn/sang/App.java
@@ -0,0 +1,10 @@
+package cn.sang;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class App {
+    public static void main(String[] args){
+        
+    }
+}
---
+++ le-search/src/main/java/cn/sang/App.java
@@ -0,0 +1,7 @@
+package cn.sang;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class App {
+}
